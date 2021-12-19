f5909a35e41eb51a0e089c58eca5262645802b51
opnaumov <o.p.naumov@gmail.com>
2021-12-20 00:37:40 +0300

    bicycle

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,11 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+let pageNavigation = document.querySelector('.navigation__list');
+let headerToggle = document.querySelector('.navigation__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+pageNavigation.classList.add('navigation--hide');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
+headerToggle.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  pageNavigation.classList.toggle('navigation--hide');
+  headerToggle.classList.toggle('navigation__toggle--close');
 });
