9f78ca0a7bf4a6e1bc1550cb5c8944c810e31b72
opnaumov <o.p.naumov@gmail.com>
2021-12-20 03:22:12 +0300

    правки

--- build/js/script.js
+++ build/js/script.js
@@ -9,13 +9,3 @@ headerToggle.addEventListener('click', function (evt) {
   pageNavigation.classList.toggle('navigation--hide');
   headerToggle.classList.toggle('navigation__toggle--close');
 });
-/*
-  if (headerToggle.classList.contains('navigation__toggle--close')) {
-    pageNavigation.classList.add('navigation--hide');
-    headerToggle.classList.remove('navigation__toggle--close');
-  } else {
-    pageNavigation.classList.remove('navigation--hide');
-    headerToggle.classList.add('navigation__toggle--close');
-  }
-
-*/
