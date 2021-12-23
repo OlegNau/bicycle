4b64a1b0d280b6c995e589deed97a8c386e94b46
opnaumov <o.p.naumov@gmail.com>
2021-12-24 02:08:29 +0300

    исправления

--- build/js/script.js
+++ build/js/script.js
@@ -1,11 +1,24 @@
-'use strict';
-let pageNavigation = document.querySelector('.navigation__list');
-let headerToggle = document.querySelector('.navigation__toggle');
+"use strict";
+let pageNavigation = document.querySelector(".navigation__list");
+let headerToggle = document.querySelector(".navigation__toggle");
+let shopBody = document.querySelector(".page__body");
+let navigationLink = document.querySelectorAll(".navigation__link")
 
-pageNavigation.classList.add('navigation--hide');
+pageNavigation.classList.add("navigation--hide");
 
-headerToggle.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  pageNavigation.classList.toggle('navigation--hide');
-  headerToggle.classList.toggle('navigation__toggle--close');
+headerToggle.addEventListener("click", function () {
+    pageNavigation.classList.toggle("navigation--hide");
+    headerToggle.classList.toggle("navigation__toggle--close");
+    shopBody.classList.toggle("page__body--hidden");
 });
+
+for (let i = 0; i < navigationLink.length; i++) {
+  let navigationLinkButton = navigationLink[i];
+  navigationLinkButton.addEventListener("click", function () {
+    if (headerToggle.classList.contains("navigation__toggle--close")) {
+        pageNavigation.classList.add("navigation--hide");
+        shopBody.classList.remove("page__body--hidden");
+        headerToggle.classList.remove("navigation__toggle--close")
+    }
+  });
+}
